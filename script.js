7fef47f3d6caaa4d8020f1c7b0349ee1b00f57e8
Droopy16a <122170977+Droopy16a@users.noreply.github.com>
2024-05-22 14:33:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -54,9 +54,9 @@ var ip = url[1];
 var ws = null;
 
 if (ip){
-    ws = new WebSocket("wss://" + ip + ":5000");
+    ws = new WebSocket("wss://" + ip + ":80");
 }else{
-    ws = new WebSocket("wss://192.168.1.26:5000");
+    ws = new WebSocket("wss://192.168.1.26:80");
 };
 
 const chat = document.getElementById("message");
