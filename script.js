215f7f98c395b5245ac489d82b110dd1c9c5ba2f
Droopy16a <122170977+Droopy16a@users.noreply.github.com>
2024-05-23 11:57:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ var ip = url[1];
 var ws = null;
 
 if (ip){
-    ws = new WebSocket("wss://" + ip);
+    ws = new WebSocket("wss://" + ip + ":5000");
 }else{
     ws = new WebSocket("wss://192.168.1.26:5000");
 };
